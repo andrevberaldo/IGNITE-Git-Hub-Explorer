af7f3fa770f9eb79cd8ab6c0af27beb714da80af
andrevberaldo <andreberaldo@hotmail.com>
2022-10-16 09:51:06 -0300

    create RepositoryList component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Repositories List</h1>
+            <ul>
+                <li>
+                    <strong>Repo Name</strong>
+                    <p>Repo Description</p>
+                    <a href="">Link to the Repo</a>
+                </li>
+
+                <li>
+                    <strong>Repo Name</strong>
+                    <p>Repo Description</p>
+                    <a href="">Link to the Repo</a>
+                </li>
+
+                <li>
+                    <strong>Repo Name</strong>
+                    <p>Repo Description</p>
+                    <a href="">Link to the Repo</a>
+                </li>
+            </ul>
+        </section>
+    );
+}
