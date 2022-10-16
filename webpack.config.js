95770c9f6c0850cd7ac6d4df17e7982f0144c86c
andrevberaldo <andreberaldo@hotmail.com>
2022-10-16 09:05:23 -0300

    add cross-env tool and scripts into package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path'); //lib que auxilia o path do projeto
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'), //arquivo principal do projeto
     output: {
         path: path.resolve(__dirname, 'dist'), //qual a pasta será o output
