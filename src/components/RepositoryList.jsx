a93f360599c946b9ea143e5a877b07c6284af62b
andrevberaldo <andreberaldo@hotmail.com>
2022-10-16 10:12:14 -0300

    add faker dependency and create RepositoryItem component

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,25 +1,21 @@
+import { faker } from '@faker-js/faker';
+import { RepositoryItem } from './RepositoryItem';
+
 export function RepositoryList() {
+
+    const repository = {
+        name: faker.word.noun(),
+        description: faker.word.noun(),
+        link: faker.internet.url()
+    }
+
     return (
         <section className="repository-list">
             <h1>Repositories List</h1>
             <ul>
-                <li>
-                    <strong>Repo Name</strong>
-                    <p>Repo Description</p>
-                    <a href="">Link to the Repo</a>
-                </li>
-
-                <li>
-                    <strong>Repo Name</strong>
-                    <p>Repo Description</p>
-                    <a href="">Link to the Repo</a>
-                </li>
-
-                <li>
-                    <strong>Repo Name</strong>
-                    <p>Repo Description</p>
-                    <a href="">Link to the Repo</a>
-                </li>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
         </section>
     );
